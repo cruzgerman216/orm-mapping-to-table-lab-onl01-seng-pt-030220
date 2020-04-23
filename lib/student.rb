6d51bb0455ac512f696098b61fcd058e31ce9712
German Cruz <cruzgerman216@yahoo.com>
2020-04-23 22:55:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,10 @@
 class Student
   attr_accessor :id, :name, :grade
+
+  def initialized(name, grade)
+    @name = name 
+    @grade = grade
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
